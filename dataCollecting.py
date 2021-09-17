b0ca817e96b26176712af9419ffd4a28348998f8
Thyago Carvalho <85909861+thyagorc-edd@users.noreply.github.com>
2021-09-16 23:54:18 -0300

    Add files via upload

--- dataCollecting.py
+++ dataCollecting.py
@@ -1,15 +1,25 @@
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as f
-import pandas as 
+import pandas as pd
 
 import requests 
+
 
 import findspark
 findspark.init()
 
 spark = SparkSession.builder.getOrCreate()
 
-w 'http://repositorio.dados.gov.br/segrt/pensionistas/PENSIONISTAS_082021.zip'
+## baixar arquivo direto da fonte
+##zipUrl = 'http://repositorio.dados.gov.br/segrt/pensionistas/PENSIONISTAS_082021.zip'
+##outPath = './raw/'
+
+##req = requests.get(zipUrl)
+##dirOut = ( outPath + 'PENSIONISTAS.zip' )
+##with open(dirOut, 'wb') as f:
+##            f.write(req.content)
+##            f.close()
+            
 
 df_pensionistas = (
     spark
@@ -19,3 +29,33 @@ df_pensionistas = (
     .option('header',True)
     .load('PENSIONISTAS_082021.csv')
 )
+
+
+cols = ['NOME DO BENEFICIARIO', 
+        'NOME DO ORGAO',
+        'DATA DE NASCIMENTO',
+        'UF DA UPAG DE VINCULACAO',
+        'NATUREZA PENSAO',
+        'DATA INICIO DO BENEFICIO',
+        'DATA FIM DO BENEFICIO',
+        'RENDIMENTO LIQUIDO',
+        'PAGAMENTO SUSPENSO']
+
+
+df = df_pensionistas.select(cols)
+
+df = (df
+ .withColumnRenamed('NOME DO BENEFICIARIO','nome')
+ .withColumnRenamed('NOME DO ORGAO','orgao')
+ .withColumnRenamed('DATA DE NASCIMENTO','dtnas')
+ .withColumnRenamed('UF DA UPAG DE VINCULACAO','uf')
+ .withColumnRenamed('NATUREZA PENSAO','natpensao')
+ .withColumnRenamed('DATA INICIO DO BENEFICIO','dtiniben')
+ .withColumnRenamed('DATA FIM DO BENEFICIO','dtfimben')
+ .withColumnRenamed('RENDIMENTO LIQUIDO','rendLiquido')
+ .withColumnRenamed('PAGAMENTO SUSPENSO','pagsuspenso')
+)
+
+df = df.toPandas()
+
+df.to_csv('pensionistasfederal.csv', index=False)
